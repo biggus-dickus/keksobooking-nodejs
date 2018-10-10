bbd6bfd91f532ddd6fbeffb65a53039a222ba1aa
maxm <lynchnost@gmail.com>
2018-10-11 01:35:00 +0300

    Module4-task1: crude server complete

--- src/cli/commands/server.js
+++ src/cli/commands/server.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const colors = require(`colors`);
+const fs = require(`fs`);
 const http = require(`http`);
+const path = require(`path`);
+const url = require(`url`);
+
 
 const DEFAULT_PORT = 3000;
 
@@ -9,7 +13,7 @@ module.exports = {
   alias: [`--server`, `-s`],
   description: `Starts a local server on provided <port> (${DEFAULT_PORT} by default).`,
 
-  run: (port = DEFAULT_PORT) => {
+  run: (port = DEFAULT_PORT, enableLog = false) => {
     const hostname = `127.0.0.1`;
     const MimeTypes = {
       '.ico': `image/x-icon`,
@@ -22,12 +26,37 @@ module.exports = {
     };
 
     const server = http.createServer((req, res) => {
+      if (enableLog) {
+        console.log(`${req.method} ${req.url}`);
+      }
 
-      console.log(`${req.method} ${req.url}`);
+      const parsedUrl = url.parse(req.url);
+      const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, ``);
+      let pathname = path.join(process.cwd(), `static`, sanitizePath);
 
-      res.statusCode = 200;
-      res.setHeader(`Content-Type`, `text/html`);
-      res.end(`<h1>Hello World!</h1>`);
+      fs.exists(pathname, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.end(`File ${pathname} not found.`);
+          return;
+        }
+
+        if (fs.statSync(pathname).isDirectory()) {
+          pathname += `/index.html`;
+        }
+
+        fs.readFile(pathname, (err, data) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(`Error getting file: ${err}.`);
+            return;
+          }
+
+          const ext = path.parse(pathname).ext;
+          res.setHeader(`Content-type`, MimeTypes[ext] || `text/plain`);
+          res.end(data);
+        });
+      });
     });
 
     server.listen(port, hostname, () => {
